eec11a9879fe5e57dfad445f5ee8ead7b4da15b2
Ayman <ubuntutest933@gmail.com>
2025-11-04 22:04:53 +0100

    ajout de app FEIGN

---
+++ AppFeignClient/src/main/java/ma/emsi/dhissiayman/tp3/appfeignclient/ClientFeignApplication.java
@@ -0,0 +1,10 @@
+package ma.emsi.dhissiayman.tp3.appfeignclient;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class ClientFeignApplication{
+
+}
---
+++ AppFeignClient/src/test/java/ma/emsi/dhissiayman/tp3/appfeignclient/AppFeignClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.dhissiayman.tp3.appfeignclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFeignClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
